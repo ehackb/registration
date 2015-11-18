d5c345b19bf8350c2437468c92fc5c4e0af004ee
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-18 17:19:32 +0100

    setup migrations for competitions

---
+++ database/migrations/2015_11_18_161636_create_competitions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompetitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('competities', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->integer('max')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('competities');
+    }
+}

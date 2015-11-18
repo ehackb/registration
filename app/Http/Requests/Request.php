b09c2d9a6d13be4728a68ec359baf4c291241ea0
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-18 23:07:20 +0100

    added validation translation (WIP)

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,10 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+	public function messages()
+	{
+		return [
+			'required' => 'Het :attribute veld is verplicht.'
+		];
+	}
 }

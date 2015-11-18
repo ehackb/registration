891fd0f1e75501c74397b064393b9109ef443cf4
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-18 17:24:58 +0100

    setup models for competitions && teams

---
+++ src/Entities/Competitions/Competition.php
@@ -0,0 +1,12 @@
+<?php
+// Created by Wannes Gennar. All rights reserved
+
+namespace EhackB\Entities\Competitions;
+
+
+use Fundamentals\Entities\Models\BaseModel;
+
+class Competition extends BaseModel
+{
+	protected $table = 'competitions';
+}
---
+++ src/Entities/Teams/Team.php
@@ -0,0 +1,12 @@
+<?php
+// Created by Wannes Gennar. All rights reserved
+
+namespace EhackB\Entities\Teams;
+
+
+use Fundamentals\Entities\Models\BaseModel;
+
+class Team extends BaseModel
+{
+	protected $table = 'teams';
+}

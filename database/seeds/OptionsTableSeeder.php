e0fa8a1254174fefedf000227e9fe47b95b97449
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-18 17:30:46 +0100

    small text tweaks

--- database/seeds/OptionsTableSeeder.php
+++ database/seeds/OptionsTableSeeder.php
@@ -7,11 +7,11 @@ class OptionsTableSeeder extends \Illuminate\Database\Seeder
 	public function run()
 	{
 		\EhackB\Entities\Options\Option::create([
-			'name' => 'Ik wil ontbijten op eHackB (€5)'
+			'name' => 'Ontbijten (€5)'
 		]);
 
 		\EhackB\Entities\Options\Option::create([
-			'name' => 'Ik wil een netwerkkabel kopen (€5 10m)'
+			'name' => 'Netwerkkabel (€5 10m)'
 		]);
 	}
 }

b879a0aed346ced24042de2ad5d20ee271accbb0
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-19 12:31:22 +0100

    recompressed CSS

--- resources/views/emails/reregister.blade.php
+++ resources/views/emails/reregister.blade.php
@@ -1,2 +1,90 @@
-Uw inschrijving op eHackB is door technische redenen niet kunnen worden verwerkt.
-gelieve u opnieuw in te schrijven
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="viewport" content="width=device-width"/>
+		<!-- NOTE: external links are for testing only -->
+		<link href="http://cdn.muicss.com/mui-latest/email/mui-email-styletag.css" rel="stylesheet"/>
+		<link href="http://cdn.muicss.com/mui-latest/email/mui-email-inline.css" rel="stylesheet"/>
+		<style>
+			#outlook a{padding:0}.ReadMsgBody{width:100%}.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:100%}.mui-container-fixed{width:600px;display:block;margin:0 auto;clear:both;text-align:left;padding-left:15px;padding-right:15px}body{width:100%!important;min-width:100%;margin:0;padding:0}img{border:0 none;height:auto;line-height:100%;outline:none;text-decoration:none}a img{border:0 none}table{border-spacing:0;border-collapse:collapse}td{padding:0;text-align:left;word-break:break-word;-webkit-hyphens:auto;-moz-hyphens:auto;hyphens:auto;border-collapse:collapse!important}table,td{mso-table-lspace:0;mso-table-rspace:0}body,table,td,p,a,li,blockquote{-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}img{-ms-interpolation-mode:bicubic}body{color:#212121;font-family:"Helvetica Neue",Helvetica,Arial,Verdana,"Trebuchet MS";font-weight:400;font-size:14px;line-height:1.429;letter-spacing:.001em;background-color:#FFF}a{color:#2196F3;text-decoration:none}p{margin:0 0 10px}hr{color:#e0e0e0;background-color:#e0e0e0;height:1px;border:none}.mui-body{margin:0;padding:0;height:100%;width:100%;color:#212121;font-family:"Helvetica Neue",Helvetica,Arial,Verdana,"Trebuchet MS";font-weight:400;font-size:14px;line-height:1.429;letter-spacing:.001em;background-color:#FFF}.mui-container,.mui-container-fixed{max-width:600px;display:block;margin:0 auto;clear:both;text-align:left;padding-left:15px;padding-right:15px}.mui-container-fixed{width:600px}strong{font-weight:700}h1,h2,h3{margin-top:20px;margin-bottom:10px}h4,h5,h6{margin-top:10px;margin-bottom:10px}.mui--text-display4{font-weight:300;font-size:112px;line-height:112px;color:#212121}.mui--text-display4.mui--text-black{color:#212121}.mui--text-display4.mui--text-white{color:#FFF}.mui--text-display4.mui--text-accent{color:#ff82ad}.mui--text-display3{font-weight:400;font-size:56px;line-height:56px;color:#212121}.mui--text-display3.mui--text-black{color:#212121}.mui--text-display3.mui--text-white{color:#FFF}.mui--text-display3.mui--text-accent{color:#ff82ad}.mui--text-display2{font-weight:400;font-size:45px;line-height:48px;color:#212121}.mui--text-display2.mui--text-black{color:#212121}.mui--text-display2.mui--text-white{color:#FFF}.mui--text-display2.mui--text-accent{color:#ff82ad}.mui--text-display1{font-weight:400;font-size:34px;line-height:40px;color:#212121}.mui--text-display1.mui--text-black{color:#212121}.mui--text-display1.mui--text-white{color:#FFF}.mui--text-display1.mui--text-accent{color:#ff82ad}.mui--text-headline{font-weight:400;font-size:24px;line-height:32px;color:#212121}.mui--text-headline.mui--text-black{color:#212121}.mui--text-headline.mui--text-white{color:#FFF}.mui--text-headline.mui--text-accent{color:#ff82ad}.mui--text-title{font-weight:400;font-size:20px;line-height:28px;color:#212121}.mui--text-title.mui--text-black{color:#212121}.mui--text-title.mui--text-white{color:#FFF}.mui--text-title.mui--text-accent{color:#ff82ad}.mui--text-subhead{font-weight:400;font-size:16px;line-height:24px;color:#212121}.mui--text-subhead.mui--text-black{color:#212121}.mui--text-subhead.mui--text-white{color:#FFF}.mui--text-subhead.mui--text-accent{color:#ff82ad}.mui--text-body2{font-weight:500;font-size:14px;line-height:24px;color:#212121}.mui--text-body2.mui--text-black{color:#212121}.mui--text-body2.mui--text-white{color:#FFF}.mui--text-body2.mui--text-accent{color:#ff82ad}.mui--text-body1{font-weight:400;font-size:14px;line-height:20px;color:#212121}.mui--text-body1.mui--text-black{color:#212121}.mui--text-body1.mui--text-white{color:#FFF}.mui--text-body1.mui--text-accent{color:#ff82ad}.mui--text-caption{font-weight:400;font-size:12px;line-height:16px;color:#212121}.mui--text-caption.mui--text-black{color:#212121}.mui--text-caption.mui--text-white{color:#FFF}.mui--text-caption.mui--text-accent{color:#ff82ad}.mui--text-menu{font-weight:500;font-size:13px;line-height:17px;color:#212121}.mui--text-menu.mui--text-black{color:#212121}.mui--text-menu.mui--text-white{color:#FFF}.mui--text-menu.mui--text-accent{color:#ff82ad}.mui--text-button{font-weight:500;font-size:14px;line-height:18px;text-transform:uppercase;color:#212121}.mui--text-button.mui--text-black{color:#212121}.mui--text-button.mui--text-white{color:#FFF}.mui--text-button.mui--text-accent{color:#ff82ad}.mui-btn{cursor:pointer;white-space:nowrap}a.mui-btn{font-weight:500;font-size:14px;color:#212121;line-height:14px;letter-spacing:.03em;text-transform:uppercase;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent;border-top:1px solid #FFF;border-left:1px solid #FFF;border-right:1px solid #FFF;border-bottom:1px solid #FFF;color:#212121;background-color:#FFF;display:inline-block;text-decoration:none;text-align:center;border-radius:3px;padding:10px 25px;background-color:transparent}a.mui-btn.mui-btn--raised{border-top:1px solid #f2f2f2;border-left:1px solid #e6e6e6;border-right:1px solid #e6e6e6;border-bottom:2px solid #bababa}a.mui-btn.mui-btn--flat{background-color:transparent;color:#212121;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}a.mui-btn.mui-btn--primary{border-top:1px solid #2196F3;border-left:1px solid #2196F3;border-right:1px solid #2196F3;border-bottom:1px solid #2196F3;color:#FFF;background-color:#2196F3}a.mui-btn.mui-btn--primary.mui-btn--raised{border-top:1px solid #51adf6;border-left:1px solid #2196F3;border-right:1px solid #2196F3;border-bottom:2px solid #0a6ebd}a.mui-btn.mui-btn--primary.mui-btn--flat{background-color:transparent;color:#2196F3;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}a.mui-btn.mui-btn--danger{border-top:1px solid #F44336;border-left:1px solid #F44336;border-right:1px solid #F44336;border-bottom:1px solid #F44336;color:#FFF;background-color:#F44336}a.mui-btn.mui-btn--danger.mui-btn--raised{border-top:1px solid #f77066;border-left:1px solid #F44336;border-right:1px solid #F44336;border-bottom:2px solid #d2190b}a.mui-btn.mui-btn--danger.mui-btn--flat{background-color:transparent;color:#F44336;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}a.mui-btn.mui-btn--dark{border-top:1px solid #424242;border-left:1px solid #424242;border-right:1px solid #424242;border-bottom:1px solid #424242;color:#FFF;background-color:#424242}a.mui-btn.mui-btn--dark.mui-btn--raised{border-top:1px solid #5c5c5c;border-left:1px solid #424242;border-right:1px solid #424242;border-bottom:2px solid #1c1c1c}a.mui-btn.mui-btn--dark.mui-btn--flat{background-color:transparent;color:#424242;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}a.mui-btn.mui-btn--accent{border-top:1px solid #FF4081;border-left:1px solid #FF4081;border-right:1px solid #FF4081;border-bottom:1px solid #FF4081;color:#FFF;background-color:#FF4081}a.mui-btn.mui-btn--accent.mui-btn--raised{border-top:1px solid #ff73a3;border-left:1px solid #FF4081;border-right:1px solid #FF4081;border-bottom:2px solid #f30053}a.mui-btn.mui-btn--accent.mui-btn--flat{background-color:transparent;color:#FF4081;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}table.mui-btn > tr > td,table.mui-btn > tbody > tr > td{background-color:#FFF}table.mui-btn > tr > td > a,table.mui-btn > tbody > tr > td > a{color:#212121;border-top:1px solid #FFF;border-left:1px solid #FFF;border-right:1px solid #FFF;border-bottom:1px solid #FFF}table.mui-btn.mui-btn--raised > tr > td > a,table.mui-btn.mui-btn--raised > tbody > tr > td > a{border-top:1px solid #f2f2f2;border-left:1px solid #e6e6e6;border-right:1px solid #e6e6e6;border-bottom:2px solid #bababa}table.mui-btn.mui-btn--flat > tr > td,table.mui-btn.mui-btn--flat > tbody > tr > td{background-color:transparent}table.mui-btn.mui-btn--flat > tr > td > a,table.mui-btn.mui-btn--flat > tbody > tr > td > a{color:#212121;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}table.mui-btn > tr > td,table.mui-btn > tbody > tr > td{border-radius:3px}table.mui-btn > tr > td > a,table.mui-btn > tbody > tr > td > a{font-weight:500;font-size:14px;color:#212121;line-height:14px;letter-spacing:.03em;text-transform:uppercase;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent;display:inline-block;text-decoration:none;text-align:center;border-radius:3px;padding:10px 25px;background-color:transparent}table.mui-btn.mui-btn--primary > tr > td,table.mui-btn.mui-btn--primary > tbody > tr > td{background-color:#2196F3}table.mui-btn.mui-btn--primary > tr > td > a,table.mui-btn.mui-btn--primary > tbody > tr > td > a{color:#FFF;border-top:1px solid #2196F3;border-left:1px solid #2196F3;border-right:1px solid #2196F3;border-bottom:1px solid #2196F3}table.mui-btn.mui-btn--primary.mui-btn--raised > tr > td > a,table.mui-btn.mui-btn--primary.mui-btn--raised > tbody > tr > td > a{border-top:1px solid #51adf6;border-left:1px solid #2196F3;border-right:1px solid #2196F3;border-bottom:2px solid #0a6ebd}table.mui-btn.mui-btn--primary.mui-btn--flat > tr > td,table.mui-btn.mui-btn--primary.mui-btn--flat > tbody > tr > td{background-color:transparent}table.mui-btn.mui-btn--primary.mui-btn--flat > tr > td > a,table.mui-btn.mui-btn--primary.mui-btn--flat > tbody > tr > td > a{color:#2196F3;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}table.mui-btn.mui-btn--danger > tr > td,table.mui-btn.mui-btn--danger > tbody > tr > td{background-color:#F44336}table.mui-btn.mui-btn--danger > tr > td > a,table.mui-btn.mui-btn--danger > tbody > tr > td > a{color:#FFF;border-top:1px solid #F44336;border-left:1px solid #F44336;border-right:1px solid #F44336;border-bottom:1px solid #F44336}table.mui-btn.mui-btn--danger.mui-btn--raised > tr > td > a,table.mui-btn.mui-btn--danger.mui-btn--raised > tbody > tr > td > a{border-top:1px solid #f77066;border-left:1px solid #F44336;border-right:1px solid #F44336;border-bottom:2px solid #d2190b}table.mui-btn.mui-btn--danger.mui-btn--flat > tr > td,table.mui-btn.mui-btn--danger.mui-btn--flat > tbody > tr > td{background-color:transparent}table.mui-btn.mui-btn--danger.mui-btn--flat > tr > td > a,table.mui-btn.mui-btn--danger.mui-btn--flat > tbody > tr > td > a{color:#F44336;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}table.mui-btn.mui-btn--dark > tr > td,table.mui-btn.mui-btn--dark > tbody > tr > td{background-color:#424242}table.mui-btn.mui-btn--dark > tr > td > a,table.mui-btn.mui-btn--dark > tbody > tr > td > a{color:#FFF;border-top:1px solid #424242;border-left:1px solid #424242;border-right:1px solid #424242;border-bottom:1px solid #424242}table.mui-btn.mui-btn--dark.mui-btn--raised > tr > td > a,table.mui-btn.mui-btn--dark.mui-btn--raised > tbody > tr > td > a{border-top:1px solid #5c5c5c;border-left:1px solid #424242;border-right:1px solid #424242;border-bottom:2px solid #1c1c1c}table.mui-btn.mui-btn--dark.mui-btn--flat > tr > td,table.mui-btn.mui-btn--dark.mui-btn--flat > tbody > tr > td{background-color:transparent}table.mui-btn.mui-btn--dark.mui-btn--flat > tr > td > a,table.mui-btn.mui-btn--dark.mui-btn--flat > tbody > tr > td > a{color:#424242;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}table.mui-btn.mui-btn--accent > tr > td,table.mui-btn.mui-btn--accent > tbody > tr > td{background-color:#FF4081}table.mui-btn.mui-btn--accent > tr > td > a,table.mui-btn.mui-btn--accent > tbody > tr > td > a{color:#FFF;border-top:1px solid #FF4081;border-left:1px solid #FF4081;border-right:1px solid #FF4081;border-bottom:1px solid #FF4081}table.mui-btn.mui-btn--accent.mui-btn--raised > tr > td > a,table.mui-btn.mui-btn--accent.mui-btn--raised > tbody > tr > td > a{border-top:1px solid #ff73a3;border-left:1px solid #FF4081;border-right:1px solid #FF4081;border-bottom:2px solid #f30053}table.mui-btn.mui-btn--accent.mui-btn--flat > tr > td,table.mui-btn.mui-btn--accent.mui-btn--flat > tbody > tr > td{background-color:transparent}table.mui-btn.mui-btn--accent.mui-btn--flat > tr > td > a,table.mui-btn.mui-btn--accent.mui-btn--flat > tbody > tr > td > a{color:#FF4081;border-top:1px solid transparent;border-left:1px solid transparent;border-right:1px solid transparent;border-bottom:1px solid transparent}a.mui-btn--small,table.mui-btn--small > tr > td > a,table.mui-btn--small > tbody > tr > td > a{font-size:13px;padding:7.8px 15px}a.mui-btn--large,table.mui-btn--large > tr > td > a,table.mui-btn--large > tbody > tr > td > a{font-size:14px;padding:19px 25px}.mui-panel{padding:15px;border-radius:0;background-color:#FFF;border-top:1px solid #ededed;border-left:1px solid #e6e6e6;border-right:1px solid #e6e6e6;border-bottom:2px solid #d4d4d4}.mui-divider{display:block;height:1px;background-color:#e0e0e0}.mui--divider-top{border-top:1px solid #e0e0e0}.mui--divider-bottom{border-bottom:1px solid #e0e0e0}.mui--divider-left{border-left:1px solid #e0e0e0}.mui--divider-right{border-right:1px solid #e0e0e0}.mui--text-left{text-align:left}.mui--text-right{text-align:right}.mui--text-center{text-align:center}.mui--text-justify{text-align:justify}.mui-image--fix{display:block}body,.mui-body{background-color:#eee;font-size:15px}.mui-container,.mui-container-fixed{padding-top:15px;padding-bottom:15px}#content-wrapper h2{margin-top:0;margin-bottom:0}#content-wrapper > tbody > tr > td{padding-bottom:15px}#content-wrapper .mui--divider-top{padding-top:15px}#last-cell{padding-bottom:15px}
+		</style>
+	</head>
+	<body>
+		<table class="mui-body" cellpadding="0" cellspacing="0" border="0">
+			<tr>
+				<td>
+					<center>
+						<!--[if mso]>
+						<table>
+							<tr>
+								<td class="mui-container-fixed"><![endif]-->
+						<div class="mui-container">
+							<h3 class="mui--text-center">
+								<img style="max-width: 10rem;" src="http://hack.ehb.be/assets/img/logoV3.png"
+									 alt="ehackb logo">
+							</h3>
+							<table cellpadding="0" cellspacing="0" border="0" width="100%">
+								<tr>
+									<td class="mui-panel">
+										<table
+												id="content-wrapper"
+												border="0"
+												cellpadding="0"
+												cellspacing="0"
+												width="100%"
+										>
+											<tbody>
+												<tr>
+													<td>
+														<h2>Welkom op EHACKB!</h2>
+													</td>
+												</tr>
+												<tr>
+													<td class="mui--divider-top">
+														Hallo {{ $registration->fname }},
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<p>Bedankt voor het inschrijven voor EHACKB!</p>
+
+														<p>U heeft de volgende opties aangeduid:</p>
+														<ul>
+															@foreach ($registration->options as $option)
+																<li>{{ $option->name }}</li>
+															@endforeach
+														</ul>
+														<p>U heeft ook aangegeven deel te nemen aan volgende
+															activiteiten:</p>
+														<ul>
+															@foreach ($registration->activities as $activity)
+																<li>{{ $activity->name }}</li>
+															@endforeach
+														</ul>
+														@if (!is_null($registration->team))
+															<p>En u neemt deel aan de competitie met uw
+																team {{ $registration->team->name }}</p>
+															<p>De invite token van uw team is
+																<i>{{ $registration->team->token }}</i></p>
+														@endif
+													</td>
+												</tr>
+												<tr>
+													<td id="last-cell">
+														Tot gauw, het EHACKB team
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</div>
+						<!--[if mso]></td></tr></table><![endif]-->
+					</center>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
